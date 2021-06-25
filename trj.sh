8e9a8e34dfcfd940cd0f15838c80ecc7b9f597d2
Days-Project <85882386+Days-Project@users.noreply.github.com>
2021-06-26 01:26:59 +0700

    Update trj.sh

--- trj.sh
+++ trj.sh
@@ -18,7 +18,7 @@ echo -e "                                               4. Check User Login Troj
 echo -e ""
 echo -e "══════════════════════════════════════════════════════════════════════════════════════" | lolcat
 echo -e ""
-echo -e "                              [x]  $(tput setaf 1)Exit$(tput sgr 0)    |    [b]  $(tput setaf 35)Back$(tput sgr 0)"
+echo -e "                              {x}  $(tput setaf 1)EXIT$(tput sgr 0)    |    {b}  $(tput setaf 35)BACK$(tput sgr 0)"
 echo -e ""
 read -p "      Choose what do you want :  " port
 echo -e ""
