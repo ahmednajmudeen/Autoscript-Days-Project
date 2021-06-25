56074e97abc654688ff90ad3aece3667bda7d566
Days-Project <85882386+Days-Project@users.noreply.github.com>
2021-06-25 22:47:52 +0700

    Update del-vless.sh

--- del-vless.sh
+++ del-vless.sh
@@ -4,6 +4,7 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/v2ray/vless.json")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		echo ""
 		echo "      You have no existing clients!"
+		sleep 2
 		wss
 	fi
 	echo ""
