b201fd165132c4b8fc148414b874de0a1784b2b6
Days-Project <85882386+Days-Project@users.noreply.github.com>
2021-06-25 23:14:31 +0700

    Update hapus.sh

--- hapus.sh
+++ hapus.sh
@@ -6,15 +6,20 @@ echo -e "    $(tput setaf 1)Use {CTRL+C}for cancel!$(tput sgr 0)"
 echo -e ""
 read -p "    Username SSH to Delete  : " Pengguna
 echo -e ""
-
-if getent passwd $Pengguna > /dev/null 2>&1; then
+if [[ "$Pengguna" = "" ]]; then
+      echo -e "    $(tput setaf 1)Please Enter a Name!$(tput sgr 0)"
+      sleep 2
+      hapus
+elif getent passwd $Pengguna > /dev/null 2>&1; then
 echo -e ""
 userdel $Pengguna
 echo -e ""
 echo -e "    $(tput setaf 1)User $Pengguna was removed. $(tput sgr 0)"
+sleep 2
+member
 else
 echo -e "    $(tput setaf 1)Failure User $Pengguna is Not Exist. $(tput sgr 0)"
+sleep 2
+hapus
 fi
 echo " "
-sleep 3
-hapus
