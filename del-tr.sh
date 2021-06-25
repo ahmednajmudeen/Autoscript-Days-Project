dd8bccf624fd04d95b21a1d2bc93061f2704b739
Days-Project <85882386+Days-Project@users.noreply.github.com>
2021-06-25 22:47:11 +0700

    Update del-tr.sh

--- del-tr.sh
+++ del-tr.sh
@@ -4,7 +4,8 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/trojan/akun.conf")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		echo ""
 		echo "      You have no existing clients!"
-		exit 1
+		sleep 2
+		trj
 	fi
 	echo ""
 	echo "  Press CTRL+C to return"
