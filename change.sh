f2d97f912fa4fdb23fb198b9048107036ec9d1a7
Days-Project <85882386+Days-Project@users.noreply.github.com>
2021-06-26 01:07:27 +0700

    Update change.sh

--- change.sh
+++ change.sh
@@ -22,7 +22,7 @@ echo -e "                                                 8. Change Port SSTP"
 echo -e ""
 echo -e "══════════════════════════════════════════════════════════════════════════════════════" | lolcat
 echo -e ""
-echo -e "                       {x}  $(tput setaf 1)EXIT$( tput sgr 0)     |      {b}   $(tput setaf 35)BACK$( tput sgr 0)"
+echo -e "                              [x]  $(tput setaf 1)EXIT$(tput sgr 0)    |    [b]  $(tput setaf 35)BACK$(tput sgr 0)"
 echo -e ""
 read -p "      What do you want : " port
 echo -e ""
